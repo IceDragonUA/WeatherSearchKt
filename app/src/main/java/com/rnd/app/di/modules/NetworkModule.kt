412410b7ce2b03983553bb2612074651dd03489d
Vladyslav Havrylenko <v.havrylenko@rndev.org>
2021-10-10 13:08:30 +0300

    log added

--- app/src/main/java/com/rnd/app/di/modules/NetworkModule.kt
+++ app/src/main/java/com/rnd/app/di/modules/NetworkModule.kt
@@ -3,13 +3,17 @@ package com.rnd.app.di.modules
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.rnd.app.BuildConfig
 import com.rnd.app.BuildConfig.BASE_URL
 import com.rnd.app.common.base.HeaderInterceptor
 import com.rnd.data.datasource.remote.api.ApiEndpoint
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+private val LOG_LEVEL = HttpLoggingInterceptor.Level.BODY
 
 val networkModule = module {
 
@@ -24,6 +28,9 @@ val networkModule = module {
 
     factory<OkHttpClient> {
         val httpClient = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            addLoggingInterceptor(httpClient, LOG_LEVEL)
+        }
         httpClient.addInterceptor(get<HeaderInterceptor>())
         httpClient.build()
     }
@@ -35,8 +42,16 @@ val networkModule = module {
     }
 }
 
+private fun addLoggingInterceptor(
+    httpClient: OkHttpClient.Builder,
+    logLevel: HttpLoggingInterceptor.Level
+) {
+    val loggingInterceptor = HttpLoggingInterceptor()
+    loggingInterceptor.level = logLevel
+    httpClient.addInterceptor(loggingInterceptor)
+}
+
 fun makeGson(
 ): Gson {
-    val gsonBuilder = GsonBuilder()
-    return gsonBuilder.create()
+    return GsonBuilder().create()
 }
--- data/src/main/java/com/rnd/data/datasource/remote/model/RemoteSearchDataResponse.kt
+++ data/src/main/java/com/rnd/data/datasource/remote/model/RemoteSearchDataResponse.kt
@@ -3,5 +3,5 @@ package com.rnd.data.datasource.remote.model
 import com.google.gson.annotations.SerializedName
 
 data class RemoteSearchDataResponse(
-    @SerializedName("id") val id: Int?
+    @SerializedName("count") val id: Int?
 )
